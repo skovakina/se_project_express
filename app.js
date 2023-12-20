9064666ead61591748e20dc991d8bf7ef72913b7
Svetlana Kovakina <skovakina@gmail.com>
2023-12-20 14:09:41 -0800

    add schemas

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
 const { PORT = 3000 } = process.env;
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.listen(PORT, () => {
   console.log(`app is runnig at port ${PORT}`);
---
+++ controllers/clothingitems.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    enum: ["hot", "warm", "cold"],
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothingitem", clothingItemSchema);
