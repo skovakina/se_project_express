183c11d8c97855ea13e798b6b953aa8e336ba74d
Svetlana Kovakina <skovakina@gmail.com>
2023-12-29 15:13:51 -0800

    feat: get curen user

--- controllers/users.js
+++ controllers/users.js
@@ -20,6 +20,27 @@ module.exports.getUsers = (req, res) => {
     );
 };
 
+module.exports.getCurrentUser = (req, res) => {
+  const { userId } = req.user._id;
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(NOT_FOUND).send({ message: "User not found" });
+      }
+      return res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res
+          .status(INVALID_DATA)
+          .send({ message: "Invalid User ID format" });
+      }
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
+    });
+};
+
 module.exports.getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const { createUser, login } = require("../controllers/users");
+const { createUser, login, getCurrentUser } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
+router.get("/users/me", auth, getCurrentUser);
 router.post("/signin", login);
 router.post("/signup", createUser);
 
