171875bc71f5d992f72d39617da98d22997e2e43
Svetlana Kovakina <skovakina@gmail.com>
2024-01-19 14:28:09 -0800

    refactor: delete comments in routers/Users.js

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,7 @@ const {
   updateUser,
 } = require("../controllers/users");
 const auth = require("../middlewares/auth");
-const {
-  validateSignup,
-  validateSignin,
-  // validateId,
-} = require("../middlewares/validation");
+const { validateSignup, validateSignin } = require("../middlewares/validation");
 
 router.get("/users/me", auth, getCurrentUser);
 router.patch("/users/me", auth, updateUser);
